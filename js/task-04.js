a38abf37b4c5ada2fee80825c46de8e554d738c5
Yurii Denkovych <yd2004@ukr.net>
2022-05-11 13:43:38 +0300

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,8 +1,8 @@
 
 const btnDecrement = document.querySelector('[data-action = "decrement"]');
-console.log(btnDecrement);
+
 const btnIncrement = document.querySelector('[data-action = "increment"]');
-console.log(btnIncrement);
+
 let valueEl = counter.querySelector('#value');
 let counterValue = 0;
 btnDecrement.addEventListener('click', function() {
