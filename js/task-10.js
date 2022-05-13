db17cbda5dacfce7de034dab5941d9b03a2d6388
Yurii Denkovych <yd2004@ukr.net>
2022-05-13 18:13:53 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -4,10 +4,7 @@ const refs = {
   btnCreate: document.querySelector('[data-create]'),
   btnDestroy: document.querySelector('[data-destroy]'),
   boxes: document.querySelector('#boxes'),
-  
-
-}
-console.log(refs.boxes);
+  }
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
@@ -16,26 +13,25 @@ function getRandomHexColor() {
 refs.inputEl.addEventListener('input',  ()=> {
   refs.btnCreate.addEventListener('click', createBoxes);
   
-
-  function createBoxes(amount){
+function createBoxes(amount){
   amount = refs.inputEl.value; 
     console.log(amount);
+    let allDiv = document.createDocumentFragment();
     for(let i = 0; i < amount; i+=1){
-     const htmlDiv =  document.createElement("div");
+      let htmlDiv =  document.createElement("div");
       let basicSize = 30; 
       let size = basicSize + i * 10;
-     htmlDiv.style.width =`${size}px`;
-     htmlDiv.style.height =`${size}px`;
-     htmlDiv.style.backgroundColor = getRandomHexColor();
-    
-    refs.boxes.append(htmlDiv);
-    console.log(refs.boxes);
+    htmlDiv.style.width =`${size}px`;
+    htmlDiv.style.height =`${size}px`;
+    htmlDiv.style.backgroundColor = getRandomHexColor();
+      allDiv.append(htmlDiv);
     }
+    refs.boxes.append(allDiv);
+    console.log(refs.boxes);
   }
 })
 
-const div = refs.boxes.querySelectorAll('div');
-console.log(div);
+
  
 
   
