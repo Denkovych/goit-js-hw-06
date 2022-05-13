6453405251024a64e8acef2567a9e10a449e592e
Yurii Denkovych <yd2004@ukr.net>
2022-05-13 18:59:32 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,15 +1,16 @@
 const styleInput = document.querySelector('style');
 const inputEl = document.querySelector('#validation-input');
 const lengthEl = inputEl.getAttribute('data-length');
+const ifLength = Number(lengthEl.value);
 
 inputEl.addEventListener('input', ()=>{
     
 inputEl.addEventListener('blur', ()=>{
-    if(inputEl.value.length < lengthEl || inputEl.value.length > lengthEl ){
+    if(inputEl.value.length < ifLength || inputEl.value.length > ifLength ){
       inputEl.classList.add('invalid');    
-    } else if(inputEl.value.length == lengthEl ){
+    } else if(inputEl.value.length == ifLength ){
         inputEl.classList.add('valid');
     }
+    
 })
 })
-console.log(lengthEl);
